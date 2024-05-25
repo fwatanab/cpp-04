9583cdbd2636f433802e0019613e37a74480521e
jabayaki <jabayaki-system>
2024-05-25 14:00:18 +0900

    Orthodox Canonical

--- ex01/WrongAnimal.hpp
+++ ex01/WrongAnimal.hpp
@@ -9,6 +9,8 @@ class	WrongAnimal{
 		std::string	type;
 	public:
 		WrongAnimal();
+		WrongAnimal(const WrongAnimal& other);
+		WrongAnimal& operator=(const WrongAnimal& other);
 		virtual ~WrongAnimal();
 		std::string	getType() const;
 		virtual void	makeSound() const;
