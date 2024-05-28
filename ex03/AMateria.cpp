0fbf1ae03853e63d16ca5c45a7280bdae6efc39f
jabayaki <jabayaki-system>
2024-05-28 11:41:40 +0900

    ADD debug print

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -1,9 +1,28 @@
 #include "AMateria.hpp"
 
-AMateria::AMateria(std::string const& type) : type(type) {}
+AMateria::AMateria() : type("none") {
+	std::cout << "AMateria default constructor" << std::endl;
+}
+
+AMateria::AMateria(std::string const& type) : type(type) {
+	std::cout << "AMateria constructor" << std::endl;
+}
+
+AMateria::AMateria(const AMateria& other) : type(other.type) {
+	std::cout << "AMateria copy constructor" << std::endl;
+}
+
+AMateria&	AMateria::operator=(const AMateria& other) {
+	if (this != &other)
+		type = other.type;
+	std::cout << "operator =" << std::endl;
+	return *this;
+}
 
 std::string const&	AMateria::getType() const {
 	return type;
 }
 
-AMateria::~AMateria() {}
+AMateria::~AMateria() {
+	std::cout << "AMateria destructor" << std::endl;
+}
--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -9,9 +9,12 @@ class	ICharacter;
 
 class	AMateria {
 	protected:
-		std::string const	type;
+		std::string	type;
 	public:
+		AMateria();
 		AMateria(std::string const& type);
+		AMateria(const AMateria& other);
+		AMateria& operator=(const AMateria& other);
 		virtual ~AMateria();
 		std::string const&	getType() const;
 		virtual AMateria*	clone() const = 0;
